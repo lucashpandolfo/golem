928b015589935c54124bd461cbd7ea00ed56d1fc
Lucas <lucashpandolfo@gmail.com>
2014-12-10 19:15:45 -0300

    Remove redundant code.

--- src/query.lisp
+++ src/query.lisp
@@ -58,14 +58,14 @@
   - validate field names
   - many-to-many"
   (declare (optimize (debug 3)))
-  (let* ((model        (get-model (row-model row)))
-         (main-model-name (model-name model))
-         (table-name   (model-table-name-as-keyword main-model-name))
-         (set-values   (copy-seq (row-columns row)))
-	 (foreign-keys (model-foreign-keys model))
-	 (m2m-keys     (model-m2m-keys model))
-         (primary-key  (model-primary-key model))
-         (primary-key-value (get-property row primary-key))
+  (let* ((model             (get-model (row-model row)))
+         (main-model-name   (model-name model))
+         (table-name        (model-table-name-as-keyword main-model-name))
+         (set-values        (copy-seq (row-columns row)))
+	 (foreign-keys      (model-foreign-keys model))
+	 (m2m-keys          (model-m2m-keys model))
+         (primary-key-name  (model-primary-key model))
+         (primary-key-value (get-property row primary-key-name))
 	 (try-insert t))
     ;; First save foreign objects recursively
     (loop :for (fkey-name . fkey-model) :in foreign-keys :do
@@ -78,51 +78,55 @@
 	     (setf (getf set-values fkey-name) fkey-value)))))
 
     ;; Then save the main object
-    (let ((set-values (copy-seq set-values)))
-      ;; Remove m2m objects
-      (loop :for (m2m-key-name . m2m-key-model) :in m2m-keys :do
-         (remf set-values m2m-key-name))
+    ;; Remove m2m objects from  the data to be saved
+    (loop :for (m2m-key-name . m2m-key-model) :in m2m-keys :do
+       (remf set-values m2m-key-name))
 
-      (when primary-key-value
-        ;; Try updating if primary key is set
-        (let* ((where-update (sxql:make-op := primary-key primary-key-value))
-               (updated (execute-with-connection
-                         (sxql:make-statement :update table-name (apply #'sxql:set= set-values)
-                                              (sxql:make-clause :where where-update)))))
-          ;; Update successfull, do not insert
-          (when (> updated 0) (setf try-insert nil))))
-      
-      (when try-insert
-        ;;Try inserting
-        (let ((inserted (execute-with-connection
-                         (sxql:make-statement :insert-into table-name (apply #'sxql:set= set-values)))))
-          (unless (> inserted 0)
-            (error "Could not insert or update the object in the database."))
-          
-          (when (not primary-key-value)
-            ;; Update primary key
-            (setf (get-property row primary-key) (last-row-id))))))
+    (when primary-key-value
+      ;; Try updating if primary key is set
+      (let* ((where-update (sxql:make-op := primary-key-name primary-key-value))
+             (updated (execute-with-connection
+                       (sxql:make-statement :update table-name (apply #'sxql:set= set-values)
+                                            (sxql:make-clause :where where-update)))))
+        ;; Update successfull, do not insert
+        (when (> updated 0) (setf try-insert nil))))
+    
+    (when try-insert
+      ;;Try inserting
+      (let ((inserted (execute-with-connection
+                       (sxql:make-statement :insert-into table-name (apply #'sxql:set= set-values)))))
+        (unless (> inserted 0)
+          (error "Could not insert or update the object in the database."))
+        
+        (when (not primary-key-value)
+          ;; Update primary key
+          (let ((new-value (last-row-id)))
+            (setf primary-key-value                   new-value
+                  (get-property row primary-key-name) new-value )))))
     
     ;; Object correctly inserted/updated. At this point the primary
     ;; key should be valid, so m2m objects can be saved.
-    (let ((main-pk (get-property row primary-key)))
-      (loop :for (m2m-key-name . m2m-model) :in m2m-keys :do
-         (let ((m2m-objects (get-property row m2m-key-name)))
-           ;; Save objects and get the saved pk's
-           (when m2m-objects
-             (let* ((saved      (mapcar #'save m2m-objects))
-                    (m2m-pk     (model-primary-key (get-model m2m-model)))
-                    (m2m-table  (model-m2m-table-name-as-keyword model m2m-key-name))
-                    (m2m-values (mapcar (lambda (s) (get-property s m2m-pk)) saved)))
-               (execute-with-connection (sxql:make-statement :delete-from m2m-table (sxql:make-clause :where (list := (model-column-name-as-keyword main-model-name) main-pk))))
-               (loop :for object :in m2m-objects
-                  :for object-key :in m2m-values :do
-                  (execute-with-connection 
-                   (sxql:make-statement 
-                    :insert-into m2m-table 
-                    (sxql:set= (model-column-name-as-keyword main-model-name) main-pk 
-                               m2m-model object-key)))
-                  (setf (get-property object m2m-pk) object-key)))))))
+    (loop :for (m2m-key-name . m2m-model) :in m2m-keys :do
+       (let ((m2m-objects (get-property row m2m-key-name)))
+         ;; Save objects and get the saved pk's
+         (when m2m-objects
+           (let* ((saved      (mapcar #'save m2m-objects))
+                  (m2m-pk     (model-primary-key (get-model m2m-model)))
+                  (m2m-table  (model-m2m-table-name-as-keyword model m2m-key-name))
+                  (m2m-values (mapcar (lambda (s) (get-property s m2m-pk)) saved)))
+             ;; Delete previous values
+             (execute-with-connection 
+              (sxql:make-statement :delete-from m2m-table 
+                                   (sxql:make-clause :where 
+                                                     (list := (model-column-name-as-keyword main-model-name) primary-key-value))))
+
+             ;; Insert new values
+             (loop :for object-key :in m2m-values :do
+                (execute-with-connection 
+                 (sxql:make-statement 
+                  :insert-into m2m-table 
+                  (sxql:set= (model-column-name-as-keyword main-model-name) primary-key-value
+                             m2m-model object-key))))))))
     )
   row)
 
